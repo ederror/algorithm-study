ced93359c5bf84b01570af3799d414f2a993b191
MinboShim <80995486+tlaalsqh7414@users.noreply.github.com>
2021-07-27 22:30:13 +0900

    Baekjoon - 2263

--- Baekjoon/2263.py
+++ Baekjoon/2263.py
@@ -1,15 +1,28 @@
-# Baekjoon - 2263
+# Baekjoon - 2263  : (실패) 메모리 초과
 def findRoot(postorder, inorder):
     global tree
+    if not postorder or not inorder:
+        return 0
     root = postorder[-1]
     idx = inorder.index(root)
-    
-    findRoot(, inorder[:idx])
-    findRoot(, inorder[idx+1:])
+    lchild = findRoot(postorder[:idx], inorder[:idx])
+    rchild = findRoot(postorder[idx:-1], inorder[idx+1:])
+    tree[root] = [lchild, rchild]
 
+    return root
+
+def preorder(root):
+    global tree
+    if root == 0:
+        return
+    
+    print(root, end=" ")
+    preorder(tree[root][0]) # left child
+    preorder(tree[root][1]) # right child
 
 n = int(input())
 inorder = list(map(int, input().split())) # LVR
 postorder = list(map(int, input().split())) # LRV
 
-tree = [[] for _ in range(n+1)]
+tree = [[0, 0] for _ in range(n+1)]
+preorder(findRoot(postorder, inorder))
---
+++ Baekjoon/2263_2.py
@@ -0,0 +1,35 @@
+# Baekjoon - 2263 
+def findRoot(post_s, post_e, inord_s, inord_e):
+    global tree, postorder, inorder
+    #if (post_s == post_e) or (inord_s == inord_e):
+    #    return 0
+
+    root = postorder[post_e-1]
+    idx = inorder.index(root)
+    lchild, rchild = 0, 0
+
+    if inord_s != idx:
+        lchild = findRoot(post_s, post_s + (idx-inord_s), inord_s, idx)
+
+    if inord_e != idx+1:
+        rchild = findRoot(post_s + (idx-inord_s), post_e-1, idx+1, inord_e)
+   
+    tree[root] = [lchild, rchild]
+
+    return root
+
+def preorder(root):
+    global tree
+    
+    print(root, end=" ")
+    if tree[root][0] != 0:
+        preorder(tree[root][0]) # left child
+    if tree[root][1] != 0:
+        preorder(tree[root][1]) # right child
+
+n = int(input())
+inorder = list(map(int, input().split())) # LVR
+postorder = list(map(int, input().split())) # LRV
+
+tree = [[0, 0] for _ in range(n+1)]
+preorder(findRoot(0, len(postorder), 0, len(inorder)))
