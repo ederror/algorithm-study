e18763824f3c90e21233f71b09fde46bb40f9518
MinboShim <80995486+tlaalsqh7414@users.noreply.github.com>
2021-08-24 17:16:35 +0900

    Programmers - 프렌즈4블록

---
+++ Programmers/프렌즈4블록.py
@@ -0,0 +1,35 @@
+# Programmers - 프렌즈4블록
+def solution(m, n, board):
+    answer = 0
+    board = [[board[j][i] for j in range(m)] for i in range(n)] # n x m
+    
+    while True:
+        toBeDeleted = set([])
+        
+        for i in range(n-1):
+            for j in range(m-1):
+                if (board[i][j:j+2]+board[i+1][j:j+2]).count(board[i][j]) == 4 and board[i][j] != "X":
+                    toBeDeleted.add((i,j))
+                    toBeDeleted.add((i+1,j))
+                    toBeDeleted.add((i,j+1))
+                    toBeDeleted.add((i+1,j+1))
+                
+        if not toBeDeleted:
+            break
+            
+        answer += len(toBeDeleted)
+        
+        for i, j in toBeDeleted:
+            board[i][j] = 'X'
+        
+        for i in range(n): # column
+            j = len(board[i])-1
+            for __ in range(m):
+                if board[i][j] == 'X':
+                    for k in range(j-1, -1, -1):
+                        board[i][k+1] = board[i][k]
+                    board[i][0] = 'X'
+                else:
+                    j -= 1
+                    
+    return answer
